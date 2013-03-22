23614d8a17b41b7bdb0e811a78856e43e105f2de
Mike Rojas <mike@ayalogic.com>
2013-03-22 17:34:49 -0400

    Created base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3BootstrapDeviseCancan::Application.config.secret_token = '58626c157e47f15ddbdbe8553037d047761a572fd448f00a7b3d99eb69d7b0f7d1985235e199f8b82674296a5dcdad6edfee2fceaad25bca2abe7a6b70c31cea'
